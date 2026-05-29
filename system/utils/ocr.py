5ba2d662ae7891194a7407688051f9ce3681acc4
kavinda9 <99938424+kavinda9@users.noreply.github.com>
2026-05-29 15:44:08 +0530

    updated ocr.py

--- system/utils/ocr.py
+++ system/utils/ocr.py
@@ -2,6 +2,9 @@
 Module for extracting the text from an image using optical-character-recognition.
 """
 import string
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import cv2
 import numpy as np
